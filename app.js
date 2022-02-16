742557b1e8d535a91fa938b0545766030165f3c9
Toriqul-Leon <yeitoriqul@gmail.com>
2022-02-16 20:23:26 +0600

    done and dusted

---
+++ app.js
@@ -0,0 +1,8 @@
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const questions = e.currentTarget.parentElement.parentElement;
+    questions.classList.toggle("show-text");
+  });
+});
